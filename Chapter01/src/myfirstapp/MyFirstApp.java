bb51069c82929cc346d698408c4f10ab2e9cfd4c
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-10-03 21:51:00 +0700

    Add 2 print line

--- Chapter01/src/myfirstapp/MyFirstApp.java
+++ Chapter01/src/myfirstapp/MyFirstApp.java
@@ -16,6 +16,8 @@ public class MyFirstApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("I Rule!");
+        System.out.println("The World");
     }
     
 }
