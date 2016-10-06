15cb86934b86b37e34fb506e6999c823ed94f93e
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-10-06 21:00:30 +0700

    test version 3

--- Chapter01/src/test/Test.java
+++ Chapter01/src/test/Test.java
@@ -20,7 +20,10 @@ public class Test {
         int x = 0;
         int y = 0;
         while (x < 5) {
-            y = y + x;
+            y = y + 2;
+            if (y > 4) {
+                y = y -1;
+            }
             System.out.print(x + "" + y + " ");
             x = x + 1;
         }
