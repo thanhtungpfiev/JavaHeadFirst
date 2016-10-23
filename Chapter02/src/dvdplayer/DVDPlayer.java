7a7e2dff709791388bcdf02112c32a9cf218e2a4
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-10-23 21:23:41 +0700

    add dvd player example

---
+++ Chapter02/src/dvdplayer/DVDPlayer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dvdplayer;
+
+/**
+ * @author thanhtung
+ * @date Oct 23, 2016
+ */
+public class DVDPlayer {
+    boolean canRecord = false;
+    
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+
+    void playDVD() {
+        System.out.println("DVD playing");
+    }
+}
