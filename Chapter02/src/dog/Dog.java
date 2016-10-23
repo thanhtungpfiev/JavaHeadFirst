ce9aac0cbbd6041aad0c835c18f08babf149e0ff
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-10-23 20:34:09 +0700

    add dog example

---
+++ Chapter02/src/dog/Dog.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dog;
+
+/**
+ * @author thanhtung
+ * @date Oct 23, 2016
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+    
+    void bark() {
+        System.out.println("Ruff! Ruff!");
+    }
+}
---
+++ Chapter02/src/dog/DogTestDrive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dog;
+
+/**
+ * @author thanhtung
+ * @date Oct 23, 2016
+ */
+public class DogTestDrive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+
+}
