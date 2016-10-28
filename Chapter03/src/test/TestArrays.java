2addc151a457b05a52e9f944b5f64e313d6b1df1
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-10-28 21:24:09 +0700

    add test array example

---
+++ Chapter03/src/test/TestArrays.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ * @author thanhtung
+ * @date Oct 28, 2016
+ */
+public class TestArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] islands = new String[4];
+        islands[0] = "Bermuda";
+        islands[1] = "Fiji";
+        islands[2] = "Azores";
+        islands[3] = "Cozumel";
+        int[] index = new int[4];
+        index[0] = 1;
+        index[1] = 3;
+        index[2] = 0;
+        index[3] = 2;
+        int y = 0;
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+            System.out.print("island = ");
+            System.out.println(islands[ref]);
+            y = y + 1;
+        }
+    }
+
+}
