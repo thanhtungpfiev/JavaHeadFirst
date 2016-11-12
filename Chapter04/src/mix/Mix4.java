fe5762b15cd52eb855d760ef8edb7531517d1cd3
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-11-12 20:36:54 +0700

    mix4 version 1

---
+++ Chapter04/src/mix/Mix4.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mix;
+
+/**
+ * @author thanhtung
+ * @date Nov 12, 2016
+ */
+public class Mix4 {
+    int counter = 0;
+    public static void main(String[] args) {
+        int count = 0;
+        Mix4[] m4a = new Mix4[20];
+        int x = 0;
+        while (x < 9) {
+            m4a[x] = new Mix4();
+            m4a[x].counter = m4a[x].counter + 1;
+            count = count + 1;
+            count = count + m4a[x].maybeNew(x);
+            x = x + 1;
+        }
+        
+        System.out.println(count + " " + m4a[1].counter);
+    }
+
+    public int maybeNew(int index) {
+        if (index < 5) {
+            Mix4 m4 = new Mix4();
+            m4.counter =  m4.counter + 1;
+            return 1;
+        }
+        return 0;
+    }
+    
+    
+}
