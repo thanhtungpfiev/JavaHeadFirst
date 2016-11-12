0cf983e78732d9c95ec3ff44846d844c4f19bc88
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-11-12 10:27:51 +0700

    add poor dog example

---
+++ Chapter04/src/poordog/PoorDog.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poordog;
+
+/**
+ * @author thanhtung
+ * @date Nov 12, 2016
+ */
+public class PoorDog {
+    private int size;
+    private String name;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Chapter04/src/poordog/PoorDogTestDrive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poordog;
+
+/**
+ * @author thanhtung
+ * @date Nov 12, 2016
+ */
+public class PoorDogTestDrive {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PoorDog one = new PoorDog();
+        System.out.println("Dog size is " + one.getSize());
+        System.out.println("Dog name is " + one.getName());
+    }
+
+}
