15b655d84a5bd180e44840a20457fb2982d33649
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-11-13 21:00:15 +0700

    add simple dot com

---
+++ Chapter05/src/simpledotcom/SimpleDotCom.java
@@ -0,0 +1,15 @@
+package simpledotcom;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author thanhtung
+ * @date Nov 13, 2016
+ */
+public class SimpleDotCom {
+
+}
