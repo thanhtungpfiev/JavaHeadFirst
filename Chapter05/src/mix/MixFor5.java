fc783b49fb64b5754105669049c85607e9aae4d0
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-11-15 22:05:07 +0700

    mix version 1

---
+++ Chapter05/src/mix/MixFor5.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mix;
+
+/**
+ * @author thanhtung
+ * @date Nov 15, 2016
+ */
+public class MixFor5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer < 3; outer++) {
+            for (int inner = 4; inner > 1; inner--) {
+                x = x + 3;
+                y = y - 2;
+                if (x == 6) {
+                    break;
+                }
+                x = x + 3;
+            }
+            y = y - 2;
+        }
+        System.out.println(x + " " + y);
+    }
+
+}
