adbe160d1ee6d7d07512b60a21f092ae681db72b
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-12-11 15:52:39 +0700

    mixed2 v4

--- Chapter07/src/mixedmessages/Mixed2.java
+++ Chapter07/src/mixedmessages/Mixed2.java
@@ -47,9 +47,9 @@ public class Mixed2 {
         C c = new C();
         A a2 = new C();
         
-        a.m1();
-        b.m2();
-        c.m3();
+        a2.m1();
+        a2.m2();
+        a2.m3();
     }
 
 }
