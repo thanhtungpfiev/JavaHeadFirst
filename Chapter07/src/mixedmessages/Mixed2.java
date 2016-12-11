b00ead829f250492e72797d9fa748c1701e76bb4
THANHTUNG2508\thanhtung <thanhtungpfiev@gmail.com>
2016-12-11 15:46:48 +0700

    mixed2 v1

---
+++ Chapter07/src/mixedmessages/Mixed2.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mixedmessages;
+
+/**
+ * @author thanhtung
+ * @date Dec 11, 2016
+ */
+
+class A {
+    int ivar = 7;
+    void m1() {
+        System.out.print("A's m1, ");
+    }
+    void m2() {
+        System.out.print("A's m2, ");
+    }
+    void m3() {
+        System.out.print("A's m3, ");
+    }
+}
+
+class B extends A {
+    void m1() {
+        System.out.print("B's m1, ");
+    }
+}
+
+class C extends B {
+    void m3() {
+        System.out.print("C's m3, " +  (ivar + 6));
+    }
+}
+public class Mixed2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A a2 = new C();
+        
+        b.m1();
+        c.m2();
+        a.m3();
+    }
+
+}
